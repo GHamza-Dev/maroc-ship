d43c3211bb9d888e709267a6befd46b42201612c
ghamza-dev <elgassaihamzam54@gmail.com>
2022-11-21 10:12:15 +0100

    Clean up Main  class

--- src/main/java/com/example/marocship/Main.java
+++ src/main/java/com/example/marocship/Main.java
@@ -8,32 +8,5 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
         EntityManager em = emf.createEntityManager();
-        EntityTransaction et = em.getTransaction();
-
-//        Delivery delivery = new Delivery();
-//        delivery.setOrigin("Safi");
-//        delivery.setDestination("Korea");
-//
-//        delivery.setVehicleCatId(1L);
-//
-//        DeliveryManager deliveryManager = new DeliveryManager();
-//        deliveryManager.setPersonId(1);
-//
-//        delivery.setDeliveryManager(deliveryManager);
-
-//        public List<DepartementManager> getAll() {
-//            Query query = JPA.entityManager().createQuery("SELECT c FROM DepartementManager c");
-//            return query.getResultList();
-//        }
-
-//        try{
-//            et.begin();
-//            Query q = em.createQuery("SELECT d FROM Delivery d");
-//            System.out.println(((Delivery)q.getResultList().get(3)).getVehicleCategory().getType());
-////            em.persist(delivery);
-//            et.commit();
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
     }
 }
---
+++ src/main/java/com/example/marocship/dao/Manager.java
@@ -0,0 +1,30 @@
+package com.example.marocship.dao;
+
+import java.util.List;
+
+public class Manager implements Dao<Manager>{
+    @Override
+    public Manager get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Manager> getAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Manager manager) {
+
+    }
+
+    @Override
+    public void update(Manager manager, String[] params) {
+
+    }
+
+    @Override
+    public void delete(Manager manager) {
+
+    }
+}
